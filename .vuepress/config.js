29e378e9402749a23856e958e93b9d6aedc2cd34
Ashley Clarke <me@ashleyclarke.me>
2018-08-19 14:18:48 +0200

    add timezone rule

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -24,6 +24,7 @@ module.exports = {
                     'rules/ip',
                     'rules/phone',
                     'rules/subdomain',
+                    'rules/timezone',
                 ]
             }
         ]
